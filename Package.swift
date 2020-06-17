2ce7e69817bcfb51f5e2f2adc69f7104d7b22014
kiennt <kiennt@sapo.vn>
2020-06-17 17:19:29 +0700

    Fix SPM dependencies

--- Package.swift
+++ Package.swift
@@ -4,13 +4,19 @@ import PackageDescription
 let package = Package(
     name: "Lightbox",
     // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    dependencies: [
+        .package(url: "https://github.com/kientux/Imaginary.git", .upToNextMajor(from: "4.4.1")),
+    ],
     products: [
         .library(name: "Lightbox", targets: ["Lightbox"])
     ],
     targets: [
         .target(
             name: "Lightbox",
-            path: "Source"
+            path: "Source",
+            dependencies: [
+                "Imaginary",
+            ],
         )
     ]
 )
