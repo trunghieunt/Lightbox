d565b3fe605abe25e16fdef2366bc0080a2d4472
kiennt <kiennt@sapo.vn>
2020-06-17 16:56:50 +0700

    Add support for SPM

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Lightbox",
+    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "Lightbox", targets: ["Lightbox"])
+    ],
+    targets: [
+        .target(
+            name: "Lightbox",
+            path: "Lightbox"
+        )
+    ]
+)
